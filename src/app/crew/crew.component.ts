61f0ac4878d78de1e3d2cc1cf3dab3e1ccc143c1
Caroline STone <docphilstone@gmail.com>
2020-03-30 20:49:11 -0500

    attempted bonus mission :(

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
+  
+  memberBeingEdited: object = null;
+  isPartOfCrew: boolean = false;
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
@@ -18,4 +21,29 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    for (let members of this.crew) {
+        if (members["name"] === memberName) {
+          this.isPartOfCrew = true;
+        }
+    }
+    if (!this.isPartOfCrew) {
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    this.isPartOfCrew = false;
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipments: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+  equipmentBeingEdited: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(equipment: string) {
+    this.equipments.push(equipment);
+  }
+
+  remove(equipment: string) {
+    let index = this.equipments.indexOf(equipment);
+    this.equipments.splice(index, 1);
+  }
+
+  edit(equipment: string) {
+    this.equipmentBeingEdited = equipment;
+  }
+
+  save(updatedEquipment: string) {
+    let index = this.equipments.indexOf(updatedEquipment);
+    this.equipments.splice(index, 1, updatedEquipment);
+    this.equipmentBeingEdited = null;
   }
 
 }
